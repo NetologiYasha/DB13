f2f48c2b2ee71309ef26a45f2bec368c7f9e0b97
Vladimir <sas00877@gmail.com>
2024-08-18 19:02:20 +0300

    Домашнее задание к лекции «Продвинутая выборка данных»

---
+++ zadanie2.sql
@@ -0,0 +1,11 @@
+SELECT title, MAX(trak_duration) FROM trak
+
+SELECT title, trak_duration FROM trak 
+WHERE trak_duration >00:03:29;
+
+SELECT collection, year_of_release = 2018 and 2020
+
+SELECT title, From trak 
+WHERE title Not Like '% %'
+
+SELECT title = "мой" or "my"
